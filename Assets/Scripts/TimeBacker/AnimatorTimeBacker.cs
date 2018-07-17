c7bdded71dc1b3d9a70eb090aa973b6eaad93b38
handsomesnail <zc992833886@outlook.com>
2018-07-17 22:25:52 +0800

    添加时间倒流系统

---
+++ Assets/Scripts/TimeBacker/AnimatorTimeBacker.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//使用动画回退有一定的限制条件：
+//1.不能嵌套 只有一层layer 可以有BlendTree
+//2.每个State的speedMultiplier绑定名为AnimatorSpeed的float类型参数
+//3.每个Translation添加一个名为TimeBacking的bool参数为false的条件
+//4.位移相关最好只依赖于Animator中的参数，或者将其依赖其它参数的变化回退方法另外添加
+[RequireComponent(typeof(Animator)), DisallowMultipleComponent]
+public sealed class AnimatorTimeBacker : MonoBehaviour, ITimeBacker {
+
+    private LinkedList<AnimatorFrameInfo> animatorFrameInfos;//动画帧
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+        animatorFrameInfos = new LinkedList<AnimatorFrameInfo>();
+    }
+
+    private  void Start() {
+        TimeBackManager.Instance.AddTimeBacker(this);
+    }
+
+    private  void OnDestroy() {
+        TimeBackManager.Instance.RemoveTimeBacker(this);
+    }
+
+    private void Update() {
+        if (TimeBackManager.Instance.IsBacking) {
+            return;
+        }
+        if(animatorFrameInfos.Count == TimeBackManager.MaxFrameCount) {
+            animatorFrameInfos.RemoveFirst();
+        }
+        AnimatorFrameInfo animatorFrameInfo = animator.GetAnimatorFrameInfo();
+        animatorFrameInfos.AddLast(animatorFrameInfo);
+    }
+
+    private void FixedUpdate() {
+        if (TimeBackManager.Instance.IsBacking) {
+            return;
+        }
+    }
+
+
+    public void OnTimePause() {
+        animator.SetFloat("AnimatorSpeed", -1.0f);
+        animator.SetBool("TimeBacking", true);
+        enabled = false;
+    }
+
+    public void OnTimeResume() {
+        animator.SetFloat("AnimatorSpeed", 1.0f);
+        animator.SetBool("TimeBacking", false);
+        enabled = true;
+    }
+
+    public void B_Update() {
+        AnimatorFrameInfo animatorFrameInfo = animatorFrameInfos.Last.Value;
+        animatorFrameInfos.RemoveLast();
+        if (animator.GetCurrentAnimatorStateInfo(0).fullPathHash != animatorFrameInfo.StateNameHash) {
+            animator.Play(animatorFrameInfo.StateNameHash, animatorFrameInfo.LayerIndex, animatorFrameInfo.NormalizedTime);
+        }
+        animator.SetAnimatorInfo(animatorFrameInfo);
+    }
+
+}
---
+++ Assets/Scripts/TimeBacker/TimeBackUtility.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>记录每帧物理信息</summary>
+public struct PhysicFrameInfo {
+    public Vector3 Position;
+    public Quaternion Rotation;
+    public Vector3 Scale; 
+
+    public PhysicFrameInfo(Vector3 position, Quaternion rotation, Vector3 scale) {
+        this.Position = position;
+        this.Rotation = rotation;
+        this.Scale = scale;
+    }
+
+    public override string ToString() {
+        return "Position:" + Position.ToString() + "\n" + "Rotation:" + Rotation.ToString() + "\n" + "Scale:" + Scale.ToString();
+    }
+}
+
+/// <summary>记录每帧动画信息</summary>
+public struct AnimatorFrameInfo {
+    public int StateNameHash;
+    public int LayerIndex;
+    public float NormalizedTime;
+    public string Motion; 
+    public List<KeyValuePair<string,bool>> boolParameters;
+    public List<KeyValuePair<string, int>> intParameters;
+    public List<KeyValuePair<string, float>> floatParameters;
+
+    public void AddBool(string parameterName, bool value) {
+        boolParameters.Add(new KeyValuePair<string, bool>(parameterName, value));
+    }
+
+    public void AddInt(string parameterName, int value) {
+        intParameters.Add(new KeyValuePair<string, int>(parameterName, value));
+    }
+
+    public void AddFloat(string parameterName, float value) {
+        floatParameters.Add(new KeyValuePair<string, float>(parameterName, value));
+    }
+
+    public AnimatorFrameInfo(int stateNameHash, float normalizedTime) : this(stateNameHash, normalizedTime, -1, "default") { }
+
+    public AnimatorFrameInfo(int stateNameHash, float normalizedTime, int layerIndex, string motion) {
+        this.StateNameHash = stateNameHash;
+        this.LayerIndex = layerIndex;
+        this.NormalizedTime = normalizedTime;
+        this.Motion = motion;
+        boolParameters = new List<KeyValuePair<string, bool>>();
+        intParameters = new List<KeyValuePair<string, int>>();
+        floatParameters = new List<KeyValuePair<string, float>>();
+    }
+
+}
+
+
+public static class TimeBackUtility {
+
+    /// <summary>从Transform获取物理信息 </summary>
+    public static PhysicFrameInfo GetPhysicInfo(this Transform transform) {
+        return new PhysicFrameInfo(transform.position, transform.rotation, transform.localScale);
+    }
+
+    /// <summary>设置Transform的物理信息 </summary>
+    public static void SetPhysicInfo(this Transform transform, PhysicFrameInfo physicInfo) {
+        transform.position = physicInfo.Position;
+        transform.rotation = physicInfo.Rotation;
+        transform.localScale = physicInfo.Scale;
+    }
+
+    /// <summary>从Animator获取当前动画信息 </summary>
+    public static AnimatorFrameInfo GetAnimatorFrameInfo(this Animator animator) {
+        int stateHashName = animator.GetCurrentAnimatorStateInfo(0).fullPathHash;
+        float normalizedTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+        AnimatorFrameInfo animatorFrameInfo = new AnimatorFrameInfo(stateHashName, normalizedTime);
+        Array.ForEach(animator.parameters, (animatorParameter) => {
+            //特殊的状态不进行倒流
+            if (animatorParameter.name == "AnimatorSpeed"|| animatorParameter.name == "TimeBacking")
+                return;
+            switch (animatorParameter.type) {
+                case AnimatorControllerParameterType.Bool:
+                    animatorFrameInfo.AddBool(animatorParameter.name, animator.GetBool(animatorParameter.name));
+                    break;
+                case AnimatorControllerParameterType.Int:
+                    animatorFrameInfo.AddInt(animatorParameter.name, animator.GetInteger(animatorParameter.name));
+                    break;
+                case AnimatorControllerParameterType.Float:
+                    animatorFrameInfo.AddFloat(animatorParameter.name, animator.GetFloat(animatorParameter.name));
+                    break;
+                default:
+                    break;
+            }
+        });
+        return animatorFrameInfo;
+    }
+
+    /// <summary>设置Animator的信息 </summary>
+    public static void SetAnimatorInfo(this Animator animator, AnimatorFrameInfo animatorFrameInfo) {
+        animatorFrameInfo.boolParameters.ForEach((kvp) => {
+            animator.SetBool(kvp.Key, kvp.Value);
+        });
+        animatorFrameInfo.intParameters.ForEach((kvp) => {
+            animator.SetInteger(kvp.Key, kvp.Value);
+        });
+        animatorFrameInfo.floatParameters.ForEach((kvp) => {
+            animator.SetFloat(kvp.Key, kvp.Value);
+        });
+    } 
+
+}
