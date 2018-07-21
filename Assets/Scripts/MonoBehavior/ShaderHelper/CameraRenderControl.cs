0f40324b37d23f59260163865e5bc3037775d0ba
Top61ly <923953383@qq.com>
2018-07-21 21:04:44 +0800

    添加Collider 摄像机设置 部分shader Timeline组件测试

---
+++ Assets/Scripts/MonoBehavior/ShaderHelper/CameraRenderControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using DG.Tweening;
+
+public class CameraRenderControl : MonoBehaviour
+{
+    public PostProcessVolume processVolume;
+
+    private Bloom bloom;
+    private ColorGrading colorGrading;
+
+    private void Awake()
+    {
+        processVolume.profile.TryGetSettings(out bloom);
+    }
+
+    public void StartBloom(float  intensity, float duration, float times)
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/MonoBehavior/ShaderHelper/TestShader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestShader : PostEffectBase {
+    public Shader testAutoShader;
+
+    public float x = 5;
+    public float y = 5;
+
+    public Texture noiseTexture;
+
+    private Material briSatConMaterial;
+
+    public Material material
+    {
+        get
+        {
+            briSatConMaterial = CheckShaderAndCreateMaterial(testAutoShader, briSatConMaterial);
+            return briSatConMaterial;
+        }
+    }   
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (material != null)
+        {
+            material.SetTexture("_NoiseTex", noiseTexture);
+            material.SetFloat("_ScrollXSpeed", x);
+            material.SetFloat("_ScrollYSpeed", y);
+
+            Graphics.Blit(source, destination, material);
+        }
+        else
+            Graphics.Blit(source, destination);
+    }
+}
