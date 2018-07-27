a49d1a96b39d8c08b43700652e0eafda19812c10
handsomesnail <zc992833886@outlook.com>
2018-07-27 22:17:53 +0800

    分屏效果修改

--- Assets/Scripts/SceneManager/SceneManager.cs
+++ Assets/Scripts/SceneManager/SceneManager.cs
@@ -42,15 +42,10 @@ public class SceneManager : MonoBehaviour {
                 Destroy(mask);
                 completeCallback();
             });
-            Action step1Callback =()=> StartCoroutine(WaitAsyncOperationDone(asyncOperation, LoadCallBack));
+            StartCoroutine(WaitAsyncOperationDone(asyncOperation, LoadCallBack));
         });
-        while (true) {
-            Debug.Log("IsDone:" + asyncOperation.isDone + " progress:" + asyncOperation.progress);
-            if (!asyncOperation.isDone && asyncOperation.progress<0.89) {
-                Debug.Log(asyncOperation.progress);
-                yield return asyncOperation;
-            }
-            else break;
+        while (!asyncOperation.isDone) {
+            yield return asyncOperation;
         }
         Debug.Log("加载场景完成");
         Debug.Log("IsDone:" + asyncOperation.isDone + " progress:" + asyncOperation.progress);
@@ -66,9 +61,11 @@ public class SceneManager : MonoBehaviour {
 
     private IEnumerator WaitAsyncOperationDone(AsyncOperation asyncOperation, Action callBack) {
         while (!asyncOperation.isDone) {
-            Debug.Log("wait done:"+asyncOperation.progress);
+            Debug.Log("wait done");
+            Debug.Log("IsDone:" + asyncOperation.isDone + " progress:" + asyncOperation.progress);
             yield return new WaitForEndOfFrame();
         }
+        Debug.Log("IsDone:" + asyncOperation.isDone + " progress:" + asyncOperation.progress);
         callBack();
     }
 
