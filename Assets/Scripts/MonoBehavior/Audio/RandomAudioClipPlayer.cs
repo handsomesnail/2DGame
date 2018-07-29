0103027ee070730e3f9439bd5ba13e55b204ae1b
Top61ly <923953383@qq.com>
2018-07-29 12:27:05 +0800

    Audio

--- Assets/Scripts/MonoBehavior/Audio/RandomAudioClipPlayer.cs
+++ Assets/Scripts/MonoBehavior/Audio/RandomAudioClipPlayer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class RandomAudioClipPlayer : MonoBehaviour
 {
     public AudioClip[] clips;
