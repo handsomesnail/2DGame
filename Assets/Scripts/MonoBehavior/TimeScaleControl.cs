188204779fe41c562b6bca24995f8c93186e1bea
Top61ly <923953383@qq.com>
2018-07-31 20:44:43 +0800

    添加Comic 场景

---
+++ Assets/Scripts/MonoBehavior/TimeScaleControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScaleControl : MonoBehaviour
+{
+    public static TimeScaleControl Instance;
+
+    private TimeScaleControl _instance
+    {
+        get
+        {
+            if (_instance == null)
+                Instance = FindObjectOfType<TimeScaleControl>();
+            if (_instance == null)
+                Debug.LogError("不存在时间管理Singleton");
+
+            return Instance;
+        }
+    }
+
+    public void PauseTime()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    public void StarTime()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
